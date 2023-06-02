534aed97c23d0ff1cab84df02e9a65e9724c735e
Muhammad Al Fitroh <73920440+raincember@users.noreply.github.com>
2023-06-02 16:16:57 +0700

    add garbage

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const registerRoutes = require('./routes/register');
 const locationRoutes = require('./routes/location');
 const { requireAuth } = require('./middlewares/auth')
+const garbageRoutes = require('./routes/garbage');
 const app = express();
 
 app.use(express.json());
 app.use('/', registerRoutes);
 app.use('/locations', requireAuth, locationRoutes)
+//garbage
+app.use('/garbage', garbageRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
