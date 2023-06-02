42a5d8fd1e897b41beee40fcf0315b35139f1e62
Muhammad Al Fitroh <73920440+raincember@users.noreply.github.com>
2023-06-02 16:12:52 +0700

    adding garbage models

---
+++ models/garbage.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./../database');
+const uuid = require('uuid');
+
+const Garbages = sequelize.define('Garbages', {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    defaultValue: uuid.v4,
+  },
+  type: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+  },
+  url_photo: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  createdAt: {
+    field: 'created_at',
+    type: DataTypes.DATE,
+  },
+  updatedAt: {
+    field: 'updated_at',
+    type: DataTypes.DATE,
+  },
+});
+
+module.exports = Garbages;
