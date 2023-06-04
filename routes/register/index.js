be1cfcdaf51111fcd0e0e165e4854c021e8c5413
Muhammad Al Fitroh <73920440+raincember@users.noreply.github.com>
2023-06-04 17:30:05 +0700

    add upload img to gcs routes

--- routes/register/index.js
+++ routes/register/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser } = require('./../../controllers/user');
+const multer = require('multer');
+const ImgUpload = require('../../imgupload/imgUpload');
 
-router.post('/', registerUser);
+// Konfigurasi Multer
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
+
+router.post('/', upload.single('photoFile'), ImgUpload.uploadToGcs, registerController.registerUser);
 
 module.exports = router;
