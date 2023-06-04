0f450b9454e384d9472f72d41eb123e841cd93c6
Muhammad Al Fitroh <73920440+raincember@users.noreply.github.com>
2023-06-04 18:22:41 +0700

    fix mitra routes (add img to gcs)

--- routes/mitra/index.js
+++ routes/mitra/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const mitraController = require('./../../controllers/mitra');
+const multer = require('multer');
+const ImgUpload = require('../../imgupload/imgUpload');
 
-router.post('/', mitraController.registerMitra);
+// Konfigurasi Multer
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
+
+router.post('/', upload.single('photoFile'), ImgUpload.uploadToGcs, mitraController.registerMitra);
 
 module.exports = router;
