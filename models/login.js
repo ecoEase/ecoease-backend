7bc817803e2f8efa393ae33b7278da20e6c598d6
Muhammad Al Fitroh <73920440+raincember@users.noreply.github.com>
2023-06-04 19:02:37 +0700

    add login model

---
+++ models/login.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const { sequelize } = require('./../database');
+
+const Login = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set(value) {
+      const hashedPassword = bcrypt.hashSync(value, 10);
+      this.setDataValue('password', hashedPassword);
+    },
+  },
+});
+
+Login.prototype.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = Login;
