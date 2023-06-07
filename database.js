56ff3f7530c4ddad14b2da29922d284b251008e8
Muhamad Andre <73456235+MuhamadAndre10@users.noreply.github.com>
2023-06-08 05:51:42 +0700

    new branch

--- database.js
+++ database.js
@@ -1,22 +1,19 @@
+const Sequelize = require('sequelize');
 
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize(
-    'order_db',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-);
-
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
-}).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
+const sequelize = new Sequelize('order_db', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
 });
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+  });
+
 module.exports = {
-    sequelize
-}
+  sequelize,
+};
