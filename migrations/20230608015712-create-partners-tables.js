c09924672250d120922bd12729154ea6255be1c0
Muhamad Andre <73456235+MuhamadAndre10@users.noreply.github.com>
2023-06-08 22:31:10 +0700

    add cors security

---
+++ migrations/20230608015712-create-partners-tables.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('partners', {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        defaultValue: uuid.v4,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      url_photo_profile: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('partners');
+  },
+};
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const router = require('./routes');
 
 const app = express();
+
+app.use(cors());
+app.options('*', cors());
+app.options('/api/v1', cors());
 
 app.use(express.json());
 app.use('/api/v1', router);
