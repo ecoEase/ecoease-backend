5c0d3ef96cd6a3707772a49e6c52cf4df340cc41
Muhammad Al Fitroh <73920440+raincember@users.noreply.github.com>
2023-06-08 23:14:33 +0700

    add login mitra handler

---
+++ controllers/loginmitra/index.js
@@ -0,0 +1,35 @@
+const Login = require('../../models/mitra');
+const jwt = require('jsonwebtoken');
+
+// Handler untuk login user
+async function loginMitra(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    // Cari user berdasarkan email
+    const user = await Login.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(400).json({ message: 'User tidak ditemukan' });
+    }
+
+    // Membandingkan password yang diinput dengan password user yang sudah di-hash
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: 'Password salah' });
+    }
+    const secret = 'please change this secret later!';
+    const token = jwt.sign({ user }, secret, { expiresIn: '7d' });
+    const userFullData = await Login.findOne({ where: { email } });
+    // Jika login berhasil
+    res.status(200).json({ message: 'Login sukses', data: userFullData, token: token });
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ message: 'Error logging in' });
+  }
+}
+
+module.exports = {
+  loginMitra,
+};
